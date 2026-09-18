c16bd3c45c8f7a17605c0faf87402d1a37c6bcc9
Rubashhhh <2mailruba@gmail.com>
2026-09-18 19:34:33 +0530

    chore: initialize BorrowBox backend

---
+++ backend/src/main/java/com/ruba/borrowbox/BorrowboxApplication.java
@@ -0,0 +1,13 @@
+package com.ruba.borrowbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorrowboxApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ruba/borrowbox/BorrowboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruba.borrowbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorrowboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
